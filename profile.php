b6361f6244bf0f622d156a1ee4bedaa85c2992e2
nastiti-tf <nastiti.ptf@gmail.com>
2026-08-11 13:42:16 +0700

    profile-skj

---
+++ profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Profile</title>
+</head>
+<body>
+    <h1 class="name">Nastiti Siwi Ma'arif</h1>
+    <h2 class="major">System Information Student</h2>
+    <p class="about">
+        Hi, I am Siwi, and I am a <b><b>System Information Student</b></b> at SMK Telkom <br> Sidoarjo. 
+        I choose specialization in <b><b>AI Specialist, Software Development,</b></b>  or <br> <b><b>Cloud Engineer.</b></b> 
+        I also interested in <b><b>Data Science</b></b> and <b><b>Machine Learning.</b></b>
+    </p>
+    <img src="pfp.png" alt="">
+</body>
+</html>
